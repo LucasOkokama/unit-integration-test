d9e1eda5b90f6cd9723e32e8028cdb18dc9e0bf2
Lucas Kazuhiro Okokama <lucaslko429@gmail.com>
2025-03-31 09:17:01 -0300

    test: ensure max returns the second argument

--- javascript/vitest-mosh/tests/intro.test.js
+++ javascript/vitest-mosh/tests/intro.test.js
@@ -5,4 +5,8 @@ describe("max", () => {
   it("Should return the first argument if it is greater", () => {
     expect(max(2, 1)).toBe(2)
   })
+
+  it("Should return the second argument if it is greater", () => {
+    expect(max(6, 8)).toBe(8)
+  })
 })

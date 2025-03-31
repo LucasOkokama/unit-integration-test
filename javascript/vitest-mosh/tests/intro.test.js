18838c4dfc7a471d80ddb9b4f04eff882e28a3bf
Lucas Kazuhiro Okokama <lucaslko429@gmail.com>
2025-03-31 10:28:22 -0300

    test: add test cases for fizzBuzz function

--- javascript/vitest-mosh/tests/intro.test.js
+++ javascript/vitest-mosh/tests/intro.test.js
@@ -1,5 +1,5 @@
 import { describe, it, expect } from "vitest";
-import { max } from "../src/intro";
+import { fizzBuzz, max } from "../src/intro";
 
 describe("max", () => {
   it("Should return the first argument if it is greater", () => {
@@ -12,5 +12,23 @@ describe("max", () => {
 
   it("Should return the first argument if arguments are equal", () => {
     expect(max(10, 10)).toBe(10)
+  })
+})
+
+describe("fizzBuzz", () => {
+  it("Should return FizzBuzz if arg is divisible by 3 and 5", () => {
+    expect(fizzBuzz(15)).toBe("FizzBuzz")
+  })
+
+  it("Should return Fizz if arg is only divisible by 3", () => {
+    expect(fizzBuzz(9)).toBe("Fizz")
+  })
+
+  it("Should return Buzz if arg is only divisible by 5", () => {
+    expect(fizzBuzz(20)).toBe("Buzz")
+  })
+
+  it("Should return arg as a string if not divisible by 3 or 5", () => {
+    expect(fizzBuzz(17)).toBe("17")
   })
 })

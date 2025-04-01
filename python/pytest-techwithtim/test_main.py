36cf3d1a8195cbf2ab05f0d09048c14fdd24fc28
Lucas Kazuhiro Okokama <lucaslko429@gmail.com>
2025-04-01 14:58:39 -0300

    test: add unit tests for add and divide functions

--- python/pytest-techwithtim/test_main.py
+++ python/pytest-techwithtim/test_main.py
@@ -1,5 +1,18 @@
-from main import get_weather
+from main import get_weather, add, divide
+import pytest
+
 
 def teste_get_weather():
     assert get_weather(21) == "hot"
     assert get_weather(10) == "cold"
+
+
+def test_add():
+    assert add(2, 3) == 5, "2 + 3 should be 5"
+    assert add(-1, 1) == 0, "-1 + 1 should be 0"
+    assert add(0, 0) == 0, "0 + 0 should be 0"
+
+
+def test_divide():
+    with pytest.raises(ValueError, match="Cannot divide by zero"):
+        divide(10, 0)

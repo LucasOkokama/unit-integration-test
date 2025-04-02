574be5b0e13f1d39c0557e0d3cd824616d58a3db
Lucas Kazuhiro Okokama <lucaslko429@gmail.com>
2025-04-02 13:18:41 -0300

    test: refactor test_get_weather to use pytest-mock

--- python/pytest-techwithtim/test_main.py
+++ python/pytest-techwithtim/test_main.py
@@ -2,9 +2,16 @@ from main import UserManager, get_weather, add, divide, is_prime
 import pytest
 
 
-def teste_get_weather():
-    assert get_weather(21) == "hot"
-    assert get_weather(10) == "cold"
+def test_get_weather(mocker):
+    mock_get = mocker.patch("main.requests.get")
+
+    mock_get.return_value.status_code = 200
+    mock_get.return_value.json.return_value = {"temperature": 25, "condition": "Sunny"}
+
+    result = get_weather("Dubai")
+
+    assert result == {"temperature": 25, "condition": "Sunny"}
+    mock_get.assert_called_once_with("https://api.weather.com/v1/Dubai")
 
 
 def test_add():

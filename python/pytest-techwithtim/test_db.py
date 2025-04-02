fa4bad6b8ba6886f053952cebf998f8968c28e1a
Lucas Kazuhiro Okokama <lucaslko429@gmail.com>
2025-04-02 13:57:04 -0300

    test: add unit test for save_user function

--- python/pytest-techwithtim/test_db.py
+++ python/pytest-techwithtim/test_db.py
@@ -1,4 +1,4 @@
-from db import Database
+from db import Database, save_user
 import pytest
 
 
@@ -24,3 +24,15 @@ def test_delete_user(db):
     db.add_user(2, "Diego")
     db.delete_user(2)
     assert db.get_user(2) is None
+
+
+def test_save_user(mocker):
+    mock_conn = mocker.patch("sqlite3.connect")
+    mock_cursor = mock_conn.return_value.cursor.return_value
+
+    save_user("Olivia", 35)
+
+    mock_conn.assert_called_once_with("users.db")
+    mock_cursor.execute.assert_called_once_with(
+        "INSERT INTO users (name, age) VALUES (?, ?)", ("Olivia", 35)
+    )

850d37886db05bcb4f725ac1883a06a652726bac
Lucas Kazuhiro Okokama <lucaslko429@gmail.com>
2025-04-03 14:10:45 -0300

    test: add test to check cart contains added items

--- python/pytest-pixegami/test_shopping_cart.py
+++ python/pytest-pixegami/test_shopping_cart.py
@@ -5,3 +5,9 @@ def test_can_add_item_to_cart():
     cart = ShoppingCart()
     cart.add("apple")
     assert cart.size() == 1
+
+
+def test_cart_contains_added_item():
+    cart = ShoppingCart()
+    cart.add("apple")
+    assert "apple" in cart.get_items()

4249b552e8e13a69a5fede0af903d06840a7465a
Lucas Kazuhiro Okokama <lucaslko429@gmail.com>
2025-04-03 14:51:45 -0300

    test: mock ItemDatabase in total price calculation

--- python/pytest-pixegami/test_shopping_cart.py
+++ python/pytest-pixegami/test_shopping_cart.py
@@ -1,4 +1,6 @@
+from unittest.mock import Mock
 import pytest
+from item_database import ItemDatabase
 from shopping_cart import ShoppingCart
 
 
@@ -28,5 +30,13 @@ def test_cart_limits_number_of_items(cart):
 def test_can_get_total_price(cart):
     cart.add("apple")
     cart.add("orange")
-    price_map = {"apple": 1.0, "orange": 2.0}
-    assert cart.get_total_price(price_map) == 3.0
+
+    def mock_get_item(item: str):
+        if item == "apple":
+            return 1.0
+        if item == "orange":
+            return 2.0
+
+    item_database = ItemDatabase()
+    item_database.get = Mock(side_effect=mock_get_item)
+    assert cart.get_total_price(item_database) == 3.0

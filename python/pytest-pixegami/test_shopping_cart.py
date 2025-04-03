f0ff26e00c37a42c6c0c0fdf5990c4926e6f0012
Lucas Kazuhiro Okokama <lucaslko429@gmail.com>
2025-04-03 14:25:58 -0300

    test: add test to ensure cart enforces item limit

--- python/pytest-pixegami/test_shopping_cart.py
+++ python/pytest-pixegami/test_shopping_cart.py
@@ -1,13 +1,23 @@
+import pytest
 from shopping_cart import ShoppingCart
 
 
 def test_can_add_item_to_cart():
-    cart = ShoppingCart()
+    cart = ShoppingCart(5)
     cart.add("apple")
     assert cart.size() == 1
 
 
 def test_cart_contains_added_item():
-    cart = ShoppingCart()
+    cart = ShoppingCart(5)
     cart.add("apple")
     assert "apple" in cart.get_items()
+
+
+def test_cart_limits_number_of_items():
+    cart = ShoppingCart(5)
+    for _ in range(5):
+        cart.add("apple")
+
+    with pytest.raises(OverflowError):
+        cart.add("apple")

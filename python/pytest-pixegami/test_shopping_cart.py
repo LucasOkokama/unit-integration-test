dbf12aea33a73bc30a9e41685a1725a373323fdc
Lucas Kazuhiro Okokama <lucaslko429@gmail.com>
2025-04-03 14:39:25 -0300

    test: refactor tests to use pytest fixture

--- python/pytest-pixegami/test_shopping_cart.py
+++ python/pytest-pixegami/test_shopping_cart.py
@@ -2,20 +2,22 @@ import pytest
 from shopping_cart import ShoppingCart
 
 
-def test_can_add_item_to_cart():
-    cart = ShoppingCart(5)
+@pytest.fixture
+def cart():
+    return ShoppingCart(5)
+
+
+def test_can_add_item_to_cart(cart):
     cart.add("apple")
     assert cart.size() == 1
 
 
-def test_cart_contains_added_item():
-    cart = ShoppingCart(5)
+def test_cart_contains_added_item(cart):
     cart.add("apple")
     assert "apple" in cart.get_items()
 
 
-def test_cart_limits_number_of_items():
-    cart = ShoppingCart(5)
+def test_cart_limits_number_of_items(cart):
     for _ in range(5):
         cart.add("apple")
 
@@ -23,11 +25,8 @@ def test_cart_limits_number_of_items():
         cart.add("apple")
 
 
-def test_can_get_total_price():
-    cart = ShoppingCart(5)
+def test_can_get_total_price(cart):
     cart.add("apple")
     cart.add("orange")
-
     price_map = {"apple": 1.0, "orange": 2.0}
-
     assert cart.get_total_price(price_map) == 3.0

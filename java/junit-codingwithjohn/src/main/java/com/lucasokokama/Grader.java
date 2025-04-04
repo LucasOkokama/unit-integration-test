1c54a5d076aec7df4b14c316c0eab657ac4c9794
Lucas Kazuhiro Okokama <lucaslko429@gmail.com>
2025-04-04 15:55:35 -0300

    feat: add Grader class with letter grade logic

---
+++ java/junit-codingwithjohn/src/main/java/com/lucasokokama/Grader.java
@@ -0,0 +1,24 @@
+package com.lucasokokama;
+
+public class Grader {
+  public char determineLetterGrader(int numberGrade){
+    if(numberGrade < 0){
+      throw new IllegalArgumentException("Number grande cannot be a negative value");
+    }
+    if(numberGrade < 60){
+      return 'F';
+    }
+    if(numberGrade <70){
+      return 'D';
+    }
+    if(numberGrade < 80){
+      return 'C';
+    }
+    if(numberGrade < 90){
+      return 'B';
+    }
+    else {
+      return 'A';
+    }
+  }
+}

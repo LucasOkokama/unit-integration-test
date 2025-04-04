7acc409fa9a6e72052801b73e08a4ff51c67124f
Lucas Kazuhiro Okokama <lucaslko429@gmail.com>
2025-04-04 16:45:41 -0300

    test: add unit tests for User class

---
+++ java/junit-marcocodes/src/main/java/com/jetbrains/marco/User.java
@@ -0,0 +1,7 @@
+package com.jetbrains.marco;
+
+import java.time.LocalDate;
+
+public record User(String name, Integer age, Boolean blocked, LocalDate birthDate) {
+
+}
---
+++ java/junit-marcocodes/src/test/java/com/jetbrains/marco/UserTest.java
@@ -0,0 +1,35 @@
+package com.jetbrains.marco;
+
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.xmlunit.assertj.XmlAssert;
+
+import java.time.LocalDate;
+
+import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@ExtendWith(MockitoExtension.class)
+class UserTest {
+    User user = new User("Marco", 37, false, LocalDate.now().minusYears(37));
+
+    @Test
+    @DisplayName("User should be at least 18")
+    void userShouldBeAtLeast18(){
+        assertThat(user.age()).isGreaterThanOrEqualTo(18);
+
+        assertThat(user.blocked())
+                .as("User %s should be blocked", user.name())
+                .isFalse();
+
+        assertThatJson(user).isEqualTo("{\"name\":\"Marco\",\"age\":37,\"blocked\":false,\"birthDate\":[1988,4,4]}");
+
+        assertThat("<a><b attr=\"abc\"></b></a>").contains("attr=\"abc\"");
+    }
+
+    @Test
+    void userShouldBeMarco(){
+        assertThat(user.name()).startsWith("Mar");
+    }
+}

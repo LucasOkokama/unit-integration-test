a5e95e985e61e9c2124ec8240c3e3262eb8d3a3b
Lucas Kazuhiro Okokama <lucaslko429@gmail.com>
2025-04-08 19:18:56 -0300

    test: add unit test for buscarTodos method

--- java/junit-mockito-matheusdojava/src/test/java/com/pieropan/unitario/service/UsuarioServiceTest.java
+++ java/junit-mockito-matheusdojava/src/test/java/com/pieropan/unitario/service/UsuarioServiceTest.java
@@ -1,7 +1,38 @@
 package com.pieropan.unitario.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.pieropan.unitario.dto.UsuarioRequest;
+import com.pieropan.unitario.dto.UsuarioResponse;
+import com.pieropan.unitario.entity.Usuario;
+import com.pieropan.unitario.excecao.MenorIdadeException;
+import com.pieropan.unitario.repository.UsuarioRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Collections;
+import java.util.List;
+
+@ExtendWith(MockitoExtension.class)
 class UsuarioServiceTest {
 
+  @InjectMocks
+  private UsuarioService usuarioService;
+
+  @Mock
+  private UsuarioRepository usuarioRepository;
+
+  @Test
+  @DisplayName("Deve retornar uma lista com apenas um usuário")
+  public void deveRetornarUmaListaComUmUsuario(){
+    Usuario usuario = new Usuario(1L, "Matheus", "matheus@dev.com", "minhaSenha", 18);
+    Mockito.when(usuarioRepository.findAll()).thenReturn(Collections.singletonList(usuario));
+    List<UsuarioResponse> usuarios = usuarioService.buscarTodos();
+
+    Assertions.assertEquals(1, usuarios.size());
+  }
 }

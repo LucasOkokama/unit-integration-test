cb27a034517b0843aafb9a8ffd92887cfacc1021
Lucas Kazuhiro Okokama <lucaslko429@gmail.com>
2025-04-18 20:09:31 -0300

    test: add unit tests for customeragevalidator

---
+++ csharp/xunit-evertonoliveira/UnitTests/CustomerAgeValidatorTests.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using xunit_evertonoliveira;
+
+namespace UnitTests
+{
+    public class CustomerAgeValidatorTests
+    {
+        [Fact]
+        public void Given_BirthDate_When_IsNotOfLegalAge_Then_ShouldReturnFalse()
+        {
+            var birthDate = DateTime.Now;
+            var customerAgeValidator = new CustomerAgeValidator();
+
+            var ofLegalAge = customerAgeValidator.IsOfLegalAge(birthDate);
+
+            ofLegalAge.Should().BeFalse();
+        }
+
+        [Fact]
+        public void Given_BirthDate_When_IsOfLegalAge_Then_ShouldReturnFalse()
+        {
+            var birthDate = DateTime.Now.AddYears(-20);
+            var customerAgeValidator = new CustomerAgeValidator();
+
+            var ofLegalAge = customerAgeValidator.IsOfLegalAge(birthDate);
+
+            ofLegalAge.Should().BeTrue();
+        }
+    }
+}

2658ba95ce3459a40cf9396e582b68d72c795512
Lucas Kazuhiro Okokama <lucaslko429@gmail.com>
2025-04-20 20:42:24 -0300

    test: add tests for image submission with enter key
    - Test to submit an image via the Enter key after entering valid title and URL
    - Verify if the new item is added to the image list
    - Ensure the new item is stored in localStorage
    - Check if the form inputs are cleared after submission

--- javascript/cypress-erickwendel/cypress/e2e/app.cy.js
+++ javascript/cypress-erickwendel/cypress/e2e/app.cy.js
@@ -1,3 +1,5 @@
+import { strict as assert } from 'assert'
+
 class RegisterForm {
   elements = {
     titleInput: () => cy.get('#title'),
@@ -67,6 +69,75 @@ describe('Image Registration', () => {
       registerForm.elements.imageUrlInput()
         .should('have.css', 'background-image')
         .and('include', "data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 12 12' width='12' height='12' fill='none' stroke='%23dc3545'%3e%3ccircle cx='6' cy='6' r='4.5'/%3e%3cpath stroke-linejoin='round' d='M5.8 3.6h.4L6 6.5z'/%3e%3ccircle cx='6' cy='8.2' r='.6' fill='%23dc3545' stroke='none'/%3e%3c/svg%3e")
+    })
+  })
+
+  describe('Submitting an image with valid inputs using enter key', () => {
+    after(() => {
+      cy.clearAllLocalStorage()
+    })
+
+    const input = {
+      title: "Alien (1979)",
+      url: "https://wallpapers.com/images/hd/alien-1979-sci-fi-horror-movie-r3ro28y6u2pujp66.jpg"
+    }
+
+    it('Given I am on the image registration page', () => {
+      cy.visit('/')
+    })
+
+    it("Then I click the submit button", () => {
+      registerForm.clickSubmit()
+    })
+
+    it(`When I enter "${input.title}" in the title field`, () => {
+      registerForm.typeTitle(input.title)
+    })
+
+    it("Then I should see a check icon in the title field", () => {
+      registerForm.elements.titleInput()
+        .should('have.css', 'background-image')
+        .and('include', "data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 8 8'%3e%3cpath fill='%23198754' d='M2.3 6.73.6 4.53c-.4-1.04.46-1.4 1.1-.8l1.1 1.4 3.4-3.8c.6-.63 1.6-.27 1.2.7l-4 4.6c-.43.5-.8.4-1.1.1z'/%3e%3c/svg%3e")
+    })
+
+    it(`Then I enter "${input.url}" in the URL field`, () => {
+      registerForm.typeUrl(input.url)
+    })
+
+    it("Then I should see a check icon in the imageUrl field", () => {
+      registerForm.elements.imageUrlInput()
+        .should('have.css', 'background-image')
+        .and('include', "data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 8 8'%3e%3cpath fill='%23198754' d='M2.3 6.73.6 4.53c-.4-1.04.46-1.4 1.1-.8l1.1 1.4 3.4-3.8c.6-.63 1.6-.27 1.2.7l-4 4.6c-.43.5-.8.4-1.1.1z'/%3e%3c/svg%3e")
+    })
+
+    it("Then I can hit enter to submit the form", () => {
+      registerForm.elements.imageUrlInput().type("{enter}")
+    })
+
+    it("And the list of registered images should be updated with the new item", () => {
+      cy.get('#card-list .card-img').should((elements) => {
+        const lastElement = elements[elements.length - 1]
+        const lastElementSrc = lastElement.getAttribute('src')
+        assert.strictEqual(lastElementSrc, input.url)
+      })
+    })
+
+    it("And the new item should be stored in the localStorage", () => {
+      cy.getAllLocalStorage().should(localStorage => {
+
+        const localStorageItemsArray = JSON.parse(Object.values(localStorage[window.origin]))
+        const lsLastItem = localStorageItemsArray[localStorageItemsArray.length - 1]
+
+        assert.deepStrictEqual(lsLastItem, {
+          title: input.title,
+          imageUrl: input.url,
+        })
+      })
+    })
+
+    it('Then The inputs should be cleared', () => {
+      registerForm.elements.titleInput().should('have.value', '')
+      registerForm.elements.imageUrlInput().should('have.value', '')
     })
   })
 })

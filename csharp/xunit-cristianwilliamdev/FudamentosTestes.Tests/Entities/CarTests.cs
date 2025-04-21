43a16080545fff3fe53031d4fa7627c8e5a8d9a1
Lucas Kazuhiro Okokama <lucaslko429@gmail.com>
2025-04-21 18:55:33 -0300

    feat: add initial files to create tests using xunit

---
+++ csharp/xunit-cristianwilliamdev/FudamentosTestes.Tests/Entities/CarTests.cs
@@ -0,0 +1,24 @@
+﻿using FudamentosTestes.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FudamentosTestes.Tests.Entities
+{
+    public sealed class CarTests
+    {
+        [Fact]
+        public void Constructor_GivenAllParameters_ThenShouldSetThePropertiesCorrectly()
+        {
+            var expectedId = Guid.NewGuid();
+            var expectedName = "Cars name";
+
+            var car = new Car(expectedId, expectedName);
+
+            Assert.Equal(expectedId, car.Id);
+            Assert.Equal(expectedName, car.Name);
+        }
+    }
+}

c82650140b3accd733d2a9bdad72f9d00d5d5966
Lucas Kazuhiro Okokama <lucaslko429@gmail.com>
2025-04-21 19:34:28 -0300

    test: add unit test using bogus faker for car constructor

--- csharp/xunit-cristianwilliamdev/FudamentosTestes.Tests/Entities/CarTests.cs
+++ csharp/xunit-cristianwilliamdev/FudamentosTestes.Tests/Entities/CarTests.cs
@@ -1,14 +1,20 @@
-﻿using FudamentosTestes.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Bogus;
+using FluentAssertions;
+using FudamentosTestes.Entities;
+using Xunit.Abstractions;
 
 namespace FudamentosTestes.Tests.Entities
 {
     public sealed class CarTests
     {
+        private readonly Faker _faker = new("pt_BR");
+        private readonly ITestOutputHelper _testOutputHelper;
+
+        public CarTests(ITestOutputHelper testOutputHelper)
+        {
+            _testOutputHelper = testOutputHelper;
+        }
+
         [Fact]
         public void Constructor_GivenAllParameters_ThenShouldSetThePropertiesCorrectly()
         {
@@ -36,5 +42,21 @@ namespace FudamentosTestes.Tests.Entities
         }
 
 
+        [Fact]
+        public void Constructor_GivenAllParameters_ThenShouldSetThePropertiesCorrectly_UsingFaker()
+        {
+            var expectedId = Guid.NewGuid();
+            var expectedName = _faker.Vehicle.Model();
+
+            _testOutputHelper.WriteLine(expectedName);
+
+            var car = new Car(expectedId, expectedName);
+
+
+            //Assert.Equal(expectedId, car.Id);
+            //Assert.Equal(expectedName, car.Name);
+            car.Id.Should().Be(expectedId, "should be equal");
+            car.Name.Should().Be(expectedName, "should be equal");
+        }
     }
 }

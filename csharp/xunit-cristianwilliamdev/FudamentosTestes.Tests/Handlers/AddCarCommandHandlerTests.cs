1fe7897aa990da82a7a290f2b095cff2b86c0550
Lucas Kazuhiro Okokama <lucaslko429@gmail.com>
2025-04-21 23:08:05 -0300

    test: add unit test for invalid car chassis exception

---
+++ csharp/xunit-cristianwilliamdev/FudamentosTestes.Tests/Handlers/AddCarCommandHandlerTests.cs
@@ -0,0 +1,45 @@
+﻿using Bogus;
+using FluentAssertions;
+using FudamentosTestes.Db;
+using FudamentosTestes.Entities;
+using FudamentosTestes.Handlers;
+using FudamentosTestes.Handlers.Exceptions;
+using FudamentosTestes.Services;
+using Microsoft.EntityFrameworkCore;
+using NSubstitute;
+
+namespace FudamentosTestes.Tests.Handlers
+{
+    public sealed class AddCarCommandHandlerTests
+    {
+        private readonly AddCarCommandHandler _handler;
+        private readonly Faker _faker = new("pt_BR");
+        private readonly ICarChassiValidatorService _mockCarChassiValidatorService;
+
+        public AddCarCommandHandlerTests()
+        {
+            var dbContextOptions = new DbContextOptionsBuilder<AppDbContext>();
+            dbContextOptions.UseInMemoryDatabase("FundamentosTestes");
+            var dbContext = new AppDbContext(dbContextOptions.Options);
+
+            _mockCarChassiValidatorService = Substitute.For<ICarChassiValidatorService>();
+
+            _handler = new AddCarCommandHandler(_mockCarChassiValidatorService, dbContext);
+        }
+
+        [Fact]
+        public async Task Handle_GivenChassiInvalid_ThenShouldThrowException()
+        {
+            var carName = _faker.Vehicle.Model();
+            var invalidCommand = new AddCarCommand(carName);
+
+            _mockCarChassiValidatorService.CheckIfValidAsync(Arg.Any<Guid>(), Arg.Any<CancellationToken>())
+                .Returns(Task.FromResult(false));
+
+            var resultAction = () => _handler.Handle(invalidCommand, CancellationToken.None);
+
+            await resultAction.Should().ThrowAsync<InvalidChassiException>()
+            .WithMessage($"[{carName}] chassi invalido!");
+        }
+    }
+}

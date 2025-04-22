89909855d8f3235020083fbddebe2b5a6cfc9751
Lucas Kazuhiro Okokama <lucaslko429@gmail.com>
2025-04-21 22:15:40 -0300

    test: add unit test for checkifvalidasync returning true

---
+++ csharp/xunit-cristianwilliamdev/FudamentosTestes.Tests/Services/CarChassiValidatorServiceTests.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using FudamentosTestes.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FudamentosTestes.Tests.Services
+{
+    public sealed class CarChassiValidatorServiceTests
+    {
+        [Fact]
+        public async Task CheckIfValidAsync_GivenAnyParams_ShouldReturnTrueAsync()
+        {
+            var anyId = Guid.NewGuid();
+            var validator = new CarChassiValidatorService();
+
+            var result = await validator.CheckIfValidAsync(anyId, CancellationToken.None);
+
+            result.Should().BeTrue();
+        }
+    }
+}

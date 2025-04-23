27a8aa28ce3f0738dd40bdec2b6733d895b87428
Lucas Kazuhiro Okokama <lucaslko429@gmail.com>
2025-04-23 16:25:01 -0300

    feat: create in-memory appointments interface and implements it

--- javascript/vitest-rocketseat/src/use-cases/create-appointment.spec.ts
+++ javascript/vitest-rocketseat/src/use-cases/create-appointment.spec.ts
@@ -2,6 +2,7 @@ import { describe, expect, it } from "vitest";
 import { CreateAppointment } from "./create-appointment";
 import { Appointment } from "../entities/appointment";
 import { getFutureDate } from "../tests/utils/get-future-date";
+import { InMemoryAppointmentRepository } from "../repositories/in-memory/in-memory-appointments-repository";
 
 
 describe("Create appointment", () => {
@@ -9,7 +10,8 @@ describe("Create appointment", () => {
     const startsAt = getFutureDate('2022-08-10')
     const endsAt = getFutureDate('2022-08-11')
 
-    const createAppointment = new CreateAppointment()
+    const appointmentsRepository = new InMemoryAppointmentRepository()
+    const createAppointment = new CreateAppointment(appointmentsRepository)
 
     expect(createAppointment.execute({
       customer: 'John Doe',
